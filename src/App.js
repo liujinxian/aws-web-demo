71f823c4a29323f6c3a55a28ba4c0af6584d8372
xianjinl <xianjinl0224@rayootech.com>
2022-07-27 16:24:01 +0800

    css change

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import './App.css';
-import {Authenticator } from '@aws-amplify/ui-react'
+import { Authenticator } from '@aws-amplify/ui-react'
 import API, { graphqlOperation } from '@aws-amplify/api';
 import { Button } from '@material-ui/core';
-import {getArAddress} from './graphql/queries'
+import { getArAddress } from './graphql/queries'
+import '@aws-amplify/ui-react/styles.css';
 function App() {
 
 
-  const ArCustomer = ()=>{
-   let data =  API.graphqlOperation(getArAddress,{Agency_id:'Az000000001',id: "A000000001"});
+  const ArCustomer = () => {
+    let data = API.graphqlOperation(getArAddress, { Agency_id: 'Az000000001', id: "A000000001" });
   }
 
   return (
     <div className="App">
- <Authenticator>
-      {({ signOut, user }) => (
-        <div className="App">
-          <p>
-            Hey {user.username}, welcome to my channel, with auth!
-          </p>
-          <button onClick={signOut}>Sign out</button>
-          <Button variant="contained" onClick={ArCustomer} >测试请求</Button>
-        </div>
-      )}
-    </Authenticator>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <div className="App">
+            <p>
+              Hey {user.username}, welcome to my channel, with auth!
+            </p>
+            <button onClick={signOut}>Sign out</button>
+            <Button variant="contained" onClick={ArCustomer} >登录成功</Button>
+          </div>
+        )}
+      </Authenticator>
     </div>
   );
 }
